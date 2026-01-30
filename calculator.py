a05e70d5266d1ef76d86848104c76551072bfec8
yromero418 <yahrome@siue.edu>
2026-01-30 10:48:12 -0600

    Adding Multiplication

--- calculator.py
+++ calculator.py
@@ -4,14 +4,18 @@ def add(a, b):
 def subtract(a, b): 
     return a - b 
 
+def mult(a,b):
+    return a*b
+
 def main(): 
     print("Simple Calculator") 
     print("1. Addition") 
     print("2. Subtraction") 
+    print("3. Multiplication")
 
-    choice = input("Choose an option (1 or 2): ") 
+    choice = input("Choose an option (1 ,2 or 3): ") 
 
-    if choice not in ("1", "2"): 
+    if choice not in ("1", "2", "3"): 
         print("Invalid choice") 
         return 
         
@@ -21,8 +25,11 @@ def main():
     if choice == "1": 
         result = add(num1, num2) 
         print("Result:", result) 
-    else: 
+    elif choice == "2":
         result = subtract(num1, num2) 
+        print("Result:", result)
+    else: 
+        result = mult(num1, num2) 
         print("Result:", result)
 
 if __name__ == "__main__": 
